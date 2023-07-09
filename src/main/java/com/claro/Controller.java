d3f8c0e729dce46be0f44988e1842976613007ce
manolete919 <manolete919@gmail.com>
2023-07-09 14:33:02 -0500

    text

--- src/main/java/com/claro/Controller.java
+++ src/main/java/com/claro/Controller.java
@@ -10,7 +10,7 @@ public class Controller {
 	@GetMapping(value = "/hello/{name}", produces = { "application/json" })
 	public String hello(@PathVariable String name) {
 
-		return "Esta es una nueva version Intance Group: " + name;
+		return "Vulnerability Scanning, hola " + name;
 
 	}
 
